5d72faaa2307e4bd06488784285bd9df03d035f7
unknown <niyomubyeyi150@gmail.com>
2026-05-19 20:17:41 +0200

    first commit

---
+++ Day-02/datatype.js
@@ -0,0 +1,50 @@
+//we are starting with premitive data type in javaScript
+
+// 1 string
+let name="john doe";
+let name2="Nadine";
+let name3="Ritha";
+
+// 2 Numbers
+
+let balance=120000.54;
+let balance2=-5000;
+let balance3=100;
+
+let y=23e5;
+let z=23e-5;
+
+// in number we have special values
+let a="BigInt(123456789012345678901234567890)";
+
+// 3 boolean
+let isActive=True;
+let inAdmin=False;
+
+// 4 undefined
+let x;
+let y= undefined;
+
+// 5 Null
+let z= null;
+
+// 6 Symbol
+let sym1= Symbol('description');
+let sym2= Symbol('description');
+console.log(sym1 === sym2); //false, each symbol is unique
+
+// 7 BigInt
+
+// non-premitive datatype
+ arrays
+ let arr=[1,2,3,4,5]
+
+ objects
+ let person={
+    name:"John Doe",
+    age:30,
+    
+ }
+ 
+
+
